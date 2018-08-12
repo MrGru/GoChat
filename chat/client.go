e900c33ad80feefd139c29c5e327b75fe585b164
MrGru <duan.hut@gmail.com>
2018-08-13 01:11:35 +0700

    init chat all in one room

---
+++ chat/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+// client represents a single chatting user .
+// socket is the websocket for this client.
+// send is a channel on which messages are sent.
+// room is a room this client is chatting in.
+type client struct {
+	socket *websocket.Conn
+	send   chan []byte
+	room   *room
+}
+
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+func (c *client) write() {
+	defer c.socket.Close()
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
